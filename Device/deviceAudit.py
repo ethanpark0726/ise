e6261724b0244947859d3e0a792cf8e5cca98a13
Ethan Park <hpar0001@shands.ufl.edu>
2020-05-27 10:54:01 -0400

    ISE devcie list audit

--- Device/deviceAudit.py
+++ Device/deviceAudit.py
@@ -1,7 +1,8 @@
 import sys
 import os
 import subprocess
-sys.path.append('./')
+import openpyxl
+import datetime
 
 from ise import ERS  # noqa E402
 from pprint import pprint  # noqa E402
@@ -13,30 +14,90 @@ def getList(iseObj):
     deviceList = list()
 
     temp = iseObj.get_devices(page=pageNumber)
+    deviceList.append(temp.get('response'))
 
     while temp.get('nextPage'):
-        deviceList.append(temp.get('response'))
         pageNumber += 1
         temp = iseObj.get_devices(page=pageNumber)
+        deviceList.append(temp.get('response'))
     
+    print("--- List gathering Complete!")
     return deviceList
 
 def getDeviceIDList(deviceList):
 
-    ipList = list()
+    deviceIDList = list()
     for i in range(len(deviceList)):
-        for j in range(20):
-            ipList.append(deviceList[i][j][0])
+        for j in range(len(deviceList[i])):
+            deviceIDList.append(deviceList[i][j][0])
+    print("--- Device ID gathering Complete!")
+    return deviceIDList
+
+def getIPList(iseObj, deviceIDList):
+    
+    ipList = list()
+
+    for deviceID in deviceIDList:
+        ipList.append(iseObj.get_device(deviceID).get('response').get('NetworkDeviceIPList')[0].get('ipaddress'))
+    print("--- Device IP gathering Complete!")
     return ipList
+
+def getPingResult(deviceIPList):
+
+    pingList = dict()
+
+    for deviceIP in deviceIPList:
+        
+        result = os.system('ping -n 1 -w 2 ' + deviceIP)
+
+        if result == 0:
+            pingList[deviceIP] = 'Okay'
+        else:
+            pingList[deviceIP] = 'Needs to check'
+    print("--- Device Ping list gathering Complete!")
+    return pingList
+
+def createExcelFile():
+    # Excel File Creation
+    nowDate = 'Report Date: ' + str(datetime.datetime.now().strftime('%Y-%m-%d'))
+    wb = openpyxl.Workbook()
+    ws = wb.active
+    ws.title = 'ISE Device Audit'
+    ws['A2'] = nowDate
+    ws['A4'] = 'Hostname'
+    ws['B4'] = 'IP Address'
+    ws['C4'] = 'Ping Status'
+
+    wb.save('ISE_Device_Audit.xlsx')
+    wb.close()
+
+def saveExcelFile(deviceIDList, deviceIPList, pingResult):
+
+    wb = openpyxl.load_workbook('ISE_Device_Audit.xlsx')
+    ws = wb.active
+    cellNumber = 5
+
+    for i in range(len(deviceIDList)):
+        ws['A' + str(cellNumber)] = deviceIDList[i]
+        ws['B' + str(cellNumber)] = deviceIPList[i]
+        ws['C' + str(cellNumber)] = pingResult.get(deviceIPList[i])
+        cellNumber += 1
+
+    wb.save('ISE_Device_Audit.xlsx')
+    wb.close()
 
 if __name__ == "__main__":
 
     ise = ERS(ise_node=uri['ise_node'], ers_user=uri['ers_user'], ers_pass=uri['ers_pass'], verify=False,
           disable_warnings=True, timeout=15)
     
-    #deviceList = getList(ise)
-    #deviceIDList = getDeviceIDList(deviceList)
-    #print(deviceIDList)
-    rep = os.system('ping -n 1 ' + '10.4.22.199')
+    createExcelFile()
 
-    print(rep)
+    deviceList = getList(ise)
+    
+    deviceIDList = getDeviceIDList(deviceList)
+    
+    deviceIPList = getIPList(ise, deviceIDList)
+    pingResult = getPingResult(deviceIPList)
+
+    saveExcelFile(deviceIDList, deviceIPList, pingResult)
